a93c7de9ae52d9aacfe0ac6aea66a1799bdc1a0c
dinghui <dinghui@hualala.com>
2018-07-20 00:01:23 +0800

    主页Tab的实现

--- App/src/main/java/com/kotlin/mall/ui/activity/MainActivity.kt
+++ App/src/main/java/com/kotlin/mall/ui/activity/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.kotlin.mall.ui.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.kotlin.mall.R
+import kotlinx.android.synthetic.main.activity_main.*
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavBar.checkMsgBadgeVisible(true)
+        bottomNavBar.checkCartBadge(20)
+        Observable.timer(3,TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { bottomNavBar.checkMsgBadgeVisible(true) }
+        Observable.timer(6,TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { bottomNavBar.checkCartBadge(0) }
     }
 }
